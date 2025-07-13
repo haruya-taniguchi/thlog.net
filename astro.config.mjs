f44511608b04141735cddcf37f02294ce292290c
Haruya Taniguchi <haruya@digitalcube.jp>
2025-07-13 17:06:31 +0900

    @astrojs/cloudflareを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+
   image: {
     domains: ["0megzcpqge.microcms.io"]
-  }
+  },
+
+  adapter: cloudflare()
 });
